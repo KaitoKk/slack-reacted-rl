3ac6c78daaef932949531c1ae2f702681e09726d
Kaito Hasegawa <kaitohasegawa@KaitonoMacBook-Air.local>
2023-11-20 10:38:01 +0900

    apikeyとdatabaseIdを引数で受け取るように変更

--- reacted/rl/rl.go
+++ reacted/rl/rl.go
@@ -1,23 +1,12 @@
 package rl
 
 import (
-	"errors"
-	"os"
 	"slack-reacted-rl/rl/clients"
 	"slack-reacted-rl/rl/models"
 	"slack-reacted-rl/rl/scraper"
 )
 
-var (
-	apiKey     = os.Getenv("NOTION_API_KEY")
-	databaseId = os.Getenv("NOTION_DATABASE_ID")
-)
-
-func rl(url string) error {
-	if err := checkVar(); err != nil {
-		return err
-	}
-
+func RL(url string, apiKey string, databaseId string) error {
 	title, err := scraper.FetchTitle(url)
 	if err != nil {
 		return err
@@ -36,16 +25,6 @@ func rl(url string) error {
 	err = c.PostArticle(article)
 	if err != nil {
 		return err
-	}
-	return nil
-}
-
-func checkVar() error {
-	if apiKey == "" {
-		return errors.New("環境変数NOTION_API_KEYがありません")
-	}
-	if databaseId == "" {
-		return errors.New("環境変数NOTION_DATABASE_IDがありません")
 	}
 	return nil
 }
